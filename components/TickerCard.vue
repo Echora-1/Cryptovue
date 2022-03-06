034f4d17aa51c1de7454cd6c155db9c6ddfc0116
Kirill.Ivanov <wonrut02@mail.ru>
2022-03-06 19:54:44 +0300

    separation into components

---
+++ components/TickerCard.vue
@@ -0,0 +1,59 @@
+<template>
+  <div
+    @click="selectedTicker"
+    :class="{
+      'border-4': isSelected,
+      'bg-red-300': isInvalid
+    }"
+    class="overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
+  >
+    <div class="px-4 py-5 sm:p-6 text-center">
+      <dt class="text-sm font-medium text-gray-500 truncate">
+        {{ tickerName }} - USD
+      </dt>
+      <dd class="mt-1 text-3xl font-semibold text-gray-900">
+        {{ tickerPrice }}
+      </dd>
+    </div>
+    <div class="w-full border-t border-gray-200"></div>
+    <delete-button @click.stop="deleteTicker" />
+  </div>
+</template>
+<script>
+import DeleteButton from "./DeleteButton";
+export default {
+  components: { DeleteButton },
+
+  props: {
+    tickerName: {
+      type: String,
+      default: ""
+    },
+
+    tickerPrice: {
+      type: Number,
+      default: 0
+    },
+
+    isInvalid: {
+      type: Boolean,
+      default: false
+    },
+
+    isSelected: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  methods: {
+    deleteTicker() {
+      this.$emit("delete-ticker");
+    },
+
+    selectedTicker() {
+      this.$emit("selected-ticker");
+    }
+  }
+};
+</script>
--- src/App.vue
+++ src/App.vue
@@ -11,62 +11,29 @@
       <template v-if="tickers.length">
         <hr class="w-full border-t border-gray-600 my-4" />
         <p>
-          <button
-            v-if="page > 1"
-            @click="page = page - 1"
-            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-          >
+          <default-button v-if="page > 1" @click="page = page - 1">
             Назад
-          </button>
-          <button
+          </default-button>
+          <default-button
             v-if="hasNextPage"
             @click="page = Number.parseInt(page) + 1"
-            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
           >
             Вперёд
-          </button>
+          </default-button>
         </p>
         <div>Фильтр: <input v-model="filter" /></div>
         <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
-          <div
+          <ticker-card
             v-for="t in paginatedTickers"
             :key="t.name"
-            @click="select(t)"
-            :class="{
-              'border-4': selectedTicker === t,
-              'bg-red-300': t.invalid
-            }"
-            class="overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
-          >
-            <div class="px-4 py-5 sm:p-6 text-center">
-              <dt class="text-sm font-medium text-gray-500 truncate">
-                {{ t.name }} - USD
-              </dt>
-              <dd class="mt-1 text-3xl font-semibold text-gray-900">
-                {{ formatPrice(t.price) }}
-              </dd>
-            </div>
-            <div class="w-full border-t border-gray-200"></div>
-            <button
-              @click.stop="handleDelete(t)"
-              class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
-            >
-              <svg
-                class="h-5 w-5"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="#718096"
-                aria-hidden="true"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                  clip-rule="evenodd"
-                ></path></svg
-              >Удалить
-            </button>
-          </div>
+            @selected-ticker="select(t)"
+            @delete-ticker="handleDelete(t)"
+            :ticker-name="t.name"
+            :ticker-price="formatPrice(t.price)"
+            :is-selected="selectedTicker === t"
+            :is-invalid="t.invalid"
+          />
         </dl>
         <hr class="w-full border-t border-gray-600 my-4" />
       </template>
@@ -84,10 +51,12 @@
 import { subscribeToTicker, unsubscribeFromTicker } from "./api";
 import AddTicker from "../components/AddTicker";
 import TickerGraph from "../components/TickerGraph";
+import TickerCard from "../components/TickerCard";
+import DefaultButton from "../components/DefaultButton";
 
 export default {
   name: "App",
-  components: {TickerGraph, AddTicker },
+  components: { DefaultButton, TickerCard, TickerGraph, AddTicker },
   data() {
     return {
       tickers: [],
@@ -97,7 +66,7 @@ export default {
       filter: "",
       allCoinsName: "",
       similarTicker: [],
-      repeatingTicker: false,
+      repeatingTicker: false
     };
   },
 
@@ -191,7 +160,8 @@ export default {
       };
 
       const isNotRecurring =
-        this.tickers.filter(item => item.name === currentTicker.name).length === 0;
+        this.tickers.filter(item => item.name === currentTicker.name).length ===
+        0;
 
       if (isNotRecurring) {
         this.tickers = [...this.tickers, currentTicker];
@@ -289,6 +259,4 @@ export default {
 };
 </script>
 
-<style src="./app.css">
-
-</style>
+<style src="./app.css"></style>
