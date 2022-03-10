df9cd7590d173485383ddf9377624038f476529f
Kirill.Ivanov <wonrut02@mail.ru>
2022-03-11 00:56:31 +0300

    refactoring styles

--- src/App.vue
+++ src/App.vue
@@ -14,10 +14,7 @@
             <label for="filter">
               Фильтр:
             </label>
-            <input
-              v-model="filter"
-              id="filter"
-            />
+            <input v-model="filter" id="filter" />
           </div>
           <div class="cryptovue__pagination">
             <default-button v-if="page > 1" @click="page = page - 1">
@@ -281,15 +278,17 @@ hr {
 
 .container {
   width: 100%;
-
-  @media (min-width: 1024px) {
-    max-width: 1024px;
+  @media (min-width: 640px) {
+    max-width: 640px;
   }
   @media (min-width: 768px) {
     max-width: 768px;
   }
-  @media (min-width: 640px) {
-    max-width: 640px;
+  @media (min-width: 1024px) {
+    max-width: 1024px;
+  }
+  @media (min-width: 1280px) {
+    max-width: 1280px;
   }
 }
 
